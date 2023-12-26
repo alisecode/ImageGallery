406816223f8cbf55620c25351692fae30a49682e
Alisa <alisa.melissa@icloud.com>
2023-12-26 08:10:56 +0200

    Initial Commit

---
+++ AlbumAnimation/AlbumAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumAnimationApp.swift
+//  AlbumAnimation
+//
+//  Created by Алиса Третьякова on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AlbumAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
